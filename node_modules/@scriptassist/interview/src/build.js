977ec66a1b7d4269d4aae20df97847c286087a8e
Mayank <vashistmayankk@gmail.com>
2025-05-07 04:10:24 +0530

    try 8

--- node_modules/@scriptassist/interview/src/build.js
+++ node_modules/@scriptassist/interview/src/build.js
@@ -1,10 +1,5 @@
+// build-vite.js
 import { build } from "vite";
-
-build().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
-import { build } from 'vite';
 
 build().catch((err) => {
   console.error(err);
